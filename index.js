ee5cf92292e2eed2fbdce62aeeb7a17896c5e7ff
Dominik Angerer <dominikangerer1@gmail.com>
2017-06-19 12:59:43 +0200

    adds quickstart with space attribute option

--- index.js
+++ index.js
@@ -45,14 +45,17 @@ if (subcommand == 'quickstart') {
       choices: [
         'No',
         'Yes'
-      ]
+      ],
+      when: function (answers) {
+        return cliAttribute == ''
+      }
     },
     {
       type: 'input',
       name: 'email',
       message: 'Enter your email address:',
       validate: function (value) {
-        email = value    
+        email = value
         if (value.length > 0) {
           return true
         }
@@ -65,7 +68,7 @@ if (subcommand == 'quickstart') {
       message: 'Define your password:',
       validate: function (value) {
         var done = this.async();
-      
+
         api.signup(email, value, (data) => {
           if (data.status == 200) {
             done(null, true)
@@ -84,7 +87,7 @@ if (subcommand == 'quickstart') {
       message: 'Enter your password:',
       validate: function (value) {
         var done = this.async();
-      
+
         api.login(email, value, (data) => {
           if (data.status == 200) {
             done(null, true)
@@ -94,7 +97,7 @@ if (subcommand == 'quickstart') {
         })
       },
       when: function (answers) {
-        return answers.has_account == 'Yes'
+        return answers.has_account == 'Yes' || cliAttribute != ''
       }
     },
     {
@@ -109,19 +112,22 @@ if (subcommand == 'quickstart') {
       },
       filter: function (val) {
         return val.replace(/\s+/g, '-').toLowerCase()
+      },
+      when: function (answers) {
+        return cliAttribute == ''
       }
     }
   ]
 
 } else if (subcommand == 'pull-components' || subcommand == 'push-components') {
-
+  
   questions = [
     {
       type: 'input',
       name: 'email',
       message: 'Enter your email address:',
       validate: function (value) {
-        email = value    
+        email = value
         if (value.length > 0) {
           return true
         }
@@ -134,7 +140,7 @@ if (subcommand == 'quickstart') {
       message: 'Enter your password:',
       validate: function (value) {
         var done = this.async();
-      
+
         api.login(email, value, (data) => {
           if (data.status == 200) {
             done(null, true)
@@ -158,6 +164,10 @@ if (subcommand == 'quickstart') {
   ]
 
 } else {
+  console.log()
+  console.log('No direct input command available - starting with default questions: ')
+  console.log()
+  
   questions = [
     {
       type: 'input',
@@ -270,7 +280,7 @@ inquirer.prompt(questions).then(function (answers) {
     var gitRepo = '';
 
     if (answers.type == 'Theme (Storyrenderer/Hosted)' || answers.type == 'Boilerplate (Selfhosted)') {
-      if (answers.custom_theme_url) { 
+      if (answers.custom_theme_url) {
         gitRepo = answers.custom_theme_url + '.git'
       } else {
         gitRepo = regex.exec(answers.theme)[1] + '.git'
@@ -287,8 +297,7 @@ inquirer.prompt(questions).then(function (answers) {
 
     var outputDir = './' + answers.name
 
-    console.log()
-    console.log('  The github repository ' + gitRepo + ' will be cloned now...')
+    console.log(chalk.green('✓') + ' - The github repository ' + gitRepo + ' will be cloned now...')
     ghdownload(gitRepo, outputDir)
       .on('error', function (err) {
         if (err.code == 'ENOTEMPTY') {
@@ -312,21 +321,20 @@ inquirer.prompt(questions).then(function (answers) {
           finalStep = 'npm run dev'
         }
 
+        console.log(chalk.green('✓') + ' - ' + chalk.white('Your Storyblok project is ready for you!'))
         console.log()
-        console.log(chalk.cyan('  Your storyblok project is ready for you!'))
-        console.log()
-        console.log(chalk.cyan('  Execute the following command to start Storyblok:'))
-        console.log('  cd ./' + answers.name + ' && npm install && ' + finalStep)
+        console.log(chalk.white('  Execute the following command to start Storyblok:'))
+        console.log(chalk.cyan('  cd ./' + answers.name + ' && npm install && ' + finalStep))
         console.log()
         console.log(chalk.white('  If you need more help, just try asking a question on stackoverflow'))
-        console.log(chalk.white('  with the [storyblok] tag'))
+        console.log(chalk.white('  with the [storyblok] tag or live-chat with us on www.storyblok.com'))
         console.log()
 
         if (answers.type == 'Theme (Storyrenderer/Hosted)' || answers.type == 'quickstart') {
           fs.renameSync(outputDir + '/_token.js', outputDir + '/token.js')
 
           if (answers.themeToken) {
-            replace(outputDir + '/token.js', {'INSERT_TOKEN': answers.themeToken})
+            replace(outputDir + '/token.js', { 'INSERT_TOKEN': answers.themeToken })
           }
 
           var spaceConfig = {}
@@ -348,59 +356,125 @@ inquirer.prompt(questions).then(function (answers) {
       })
   }
 
-  switch(subcommand) {
+  switch (subcommand) {
     case 'quickstart':
-      console.log('  Your project will be created now...')
+      var space_id = ''  
+      var parameter = cliAttribute.split('=')
+      if (parameter.length > 1 && parameter[0] == 'space') {
+        try {
+          space_id = parseInt(parameter[1])
+        } catch (e) {
+          console.log(chalk.red('Your space id should be a number - maybe check your input again'))
+          exit(401)
+        } 
+      } else {
+        console.log(chalk.red('It seems as we don\'t know your parameter "' + cliAttribute + '" - you may want to try this: '))
+        console.log(chalk.white('storyblok quickstart space=YOUR_SPACE_ID'))
+        exit(402)
+      }
+      switch (parameter[0]) {
+        case 'space':
+          console.log('  Your project will be initialized now...')
 
-      api.post('spaces', {
-        create_demo: false,
-        dup_id: 40288,
-        space: {
-          name: answers.name,
-          environments: [{name: 'Dev', location: 'http://localhost:4440/'}]
-        }
-      }, (space_res) => {
-        if (space_res.status == 200) {
-          console.log(chalk.green('  ' + answers.name + ' has been created in Storyblok!'))
+          api.put('spaces/' + space_id + '/', {
+            space: {
+              environments: [{ name: 'Dev', location: 'http://localhost:4440/' }]
+            }
+          }, (space_res) => {
+            answers.name = space_res.body.space.name.replace(/\s+/g, '-').toLowerCase()
+            if (space_res.status == 200) {
+              console.log(chalk.green('✓') + ' - Space ' + answers.name + ' updated with dev environment in Storyblok')
 
-          api.post('tokens', {}, (tokens_res) => {
-            answers.loginToken = tokens_res.body.key
+              api.post('tokens', {}, (tokens_res) => {
+                console.log(chalk.green('✓') + ' - Configuration for your Space was loaded')
+                answers.loginToken = tokens_res.body.key
 
-            api.setSpaceId(space_res.body.space.id)
-            api.get('api_keys', (keys_res) => {
-              if (keys_res.status == 200) {
-                answers.spaceId = space_res.body.space.id
-                answers.spaceDomain = space_res.body.space.domain.replace('https://', '')
-                                                                 .replace('/', '')
+                api.setSpaceId(space_res.body.space.id)
+                api.get('api_keys', (keys_res) => {
+                  if (keys_res.status == 200) {
+                    answers.spaceId = space_res.body.space.id
+                    answers.spaceDomain = space_res.body.space.domain.replace('https://', '')
+                      .replace('/', '')
 
-                var tokens = keys_res.body.api_keys.filter((token) => {
-                  return token.access == 'theme'
+                    var tokens = keys_res.body.api_keys.filter((token) => {
+                      return token.access == 'theme'
+                    })
+
+                    answers.themeToken = tokens[0].token
+
+                    console.log(chalk.green('✓') + ' - Development Environment configured (./' + answers.name + '/config.js' + ')')
+                    
+                    lastStep()
+                  } else {
+                    console.log(keys_res.body)
+                  }
                 })
-
-                answers.themeToken = tokens[0].token
-
-                lastStep()
-              } else {
-                console.log(keys_res.body)
-              }
-            })
+              })
+            } else {
+              console.log(space_res.body)
+            }
           })
-        } else {
-          console.log(space_res.body)
-        }
-      })
 
-    break;
+          break;
+        default:
+
+          console.log('  Your project will be created now...')
+
+          api.post('spaces', {
+            create_demo: false,
+            dup_id: 40288,
+            space: {
+              name: answers.name,
+              environments: [{ name: 'Dev', location: 'http://localhost:4440/' }]
+            }
+          }, (space_res) => {
+            if (space_res.status == 200) {
+              console.log(chalk.green('✓') + ' - Space ' + answers.name + ' has been created in Storyblok')
+              console.log(chalk.green('✓') + ' - Story "home" has been created in your Space')
+
+              api.post('tokens', {}, (tokens_res) => {
+                console.log(chalk.green('✓') + ' - Configuration for your Space was loaded')
+                answers.loginToken = tokens_res.body.key
+
+                api.setSpaceId(space_res.body.space.id)
+                api.get('api_keys', (keys_res) => {
+                  if (keys_res.status == 200) {
+                    answers.spaceId = space_res.body.space.id
+                    answers.spaceDomain = space_res.body.space.domain.replace('https://', '')
+                      .replace('/', '')
+
+                    var tokens = keys_res.body.api_keys.filter((token) => {
+                      return token.access == 'theme'
+                    })
+
+                    answers.themeToken = tokens[0].token
+
+                    console.log(chalk.green('✓') + ' - Development Environment configured (./' + answers.name + '/config.js' + ')')
+                    
+                    lastStep()
+                  } else {
+                    console.log(keys_res.body)
+                  }
+                })
+              })
+            } else {
+              console.log(space_res.body)
+            }
+          })
+          break;
+      }
+
+      break;
     case 'push-components':
       api.setSpaceId(answers.spaceId)
       pushComponents(api, cliAttribute)
 
-    break;
+      break;
     case 'pull-components':
       api.setSpaceId(answers.spaceId)
       pullComponents(api, cliAttribute)
 
-    break;
+      break;
     default:
       lastStep()
   }
