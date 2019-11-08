c26c876ab046b3a5fb4f5d734d35f3a2c03e8b82
Emanuel Souza <emanuelgdesouza@gmail.com>
2019-11-08 14:33:09 -0300

    refactor(api): refactor login and logout methods
    Extract the URL paths to constants.js file and use axios package instead unirest package

---
+++ src/constants.js
@@ -0,0 +1,11 @@
+const LOGIN_URL = 'https://api.storyblok.com/v1/users/login'
+
+const SIGNUP_URL = 'https://api.storyblok.com/v1/users/signup'
+
+const API_URL = 'https://api.storyblok.com/v1/'
+
+module.exports = {
+  LOGIN_URL,
+  SIGNUP_URL,
+  API_URL
+}
--- src/utils/api.js
+++ src/utils/api.js
@@ -1,37 +1,46 @@
 var unirest = require('unirest')
-var creds = require('./creds')
+const axios = require('axios')
+const creds = require('./creds')
+
+const { LOGIN_URL } = require('../constants')
 
 module.exports = {
   accessToken: '',
   spaceId: null,
 
-  login: function (email, password, callback) {
-    var login = unirest('POST', 'https://api.storyblok.com/v1/users/login')
-    login.type('json')
-    login.send({ email: email, password: password })
-    login.end((res) => {
-      if (res.status === 200) {
-        this.accessToken = res.body.access_token
-        creds.set(email, res.body.access_token)
-      }
-      callback(res)
+  async login (email, password) {
+    return axios.post(LOGIN_URL, {
+      email: email,
+      password: password
     })
+      .then(response => {
+        const token = response.data.access_token
+        this.accessToken = token
+        creds.set(email, token)
+
+        return Promise.resolve(true)
+      })
+      .catch(err => Promise.reject(err))
   },
 
-  logout: function () {
+  logout () {
     creds.set(null)
   },
 
-  signup: function (email, password, callback) {
-    var login = unirest('POST', 'https://api.storyblok.com/v1/users/signup')
-    login.type('json')
-    login.send({ user: { email: email, password: password } })
-    login.end((res) => {
-      if (res.status === 200) {
-        this.accessToken = res.body.access_token
-        creds.set(email, res.body.access_token)
-      }
-      callback(res)
+  signup: function (email, password) {
+    return new Promise((resolve, reject) => {
+      const login = unirest('POST', 'https://api.storyblok.com/v1/users/signup')
+      login.type('json')
+      login.send({ user: { email: email, password: password } })
+      login.end((res) => {
+        if (res.status === 200) {
+          this.accessToken = res.body.access_token
+          creds.set(email, res.body.access_token)
+          return resolve(res)
+        }
+
+        return reject(new Error('An error ocurred when signup' + JSON.stringify(res)))
+      })
     })
   },
 
