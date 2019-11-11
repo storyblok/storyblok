193eb2f8684672917969db1ebcfe99f6e89e944b
Emanuel Souza <emanuelgdesouza@gmail.com>
2019-11-11 18:26:11 -0300

    test(tasks): add task to quickstart function

---
+++ tests/units/quickstart.spec.js
@@ -0,0 +1,59 @@
+const fs = require('fs')
+const path = require('path')
+
+const quickstart = require('../../src/tasks/quickstart')
+const Storyblok = require('storyblok-js-client')
+const api = require('../../src/utils/api')
+const { API_URL } = require('../../src/constants')
+
+jest.unmock('fs')
+jest.unmock('axios')
+
+const TEST_PATH = path.join(__dirname, '../../space-test')
+
+describe('testing quickstart()', () => {
+  beforeEach(() => {
+    console.log('deleted', TEST_PATH)
+    fs.rmdirSync(TEST_PATH, { recursive: true })
+
+    api.setSpaceId(null)
+  })
+
+  afterEach(() => {
+    fs.rmdirSync(TEST_PATH, { recursive: true })
+
+    api.setSpaceId(null)
+  })
+
+  it('call quickstart() with spaceId should clone the quickstart repository space-test folder', async () => {
+    if (process.env.STORYBLOK_TOKEN && process.env.STORYBLOK_SPACE) {
+      api.accessToken = process.env.STORYBLOK_TOKEN
+
+      await quickstart(api, {}, process.env.STORYBLOK_SPACE)
+
+      expect(fs.existsSync(TEST_PATH)).toBe(true)
+    }
+  })
+
+  it('call quickstart() without spaceId should create an empty space with corresponding name', async () => {
+    const TEST_NAME = 'testando'
+    if (process.env.STORYBLOK_TOKEN) {
+      api.accessToken = process.env.STORYBLOK_TOKEN
+
+      await quickstart(api, { name: TEST_NAME })
+
+      const client = new Storyblok({
+        oauthToken: process.env.STORYBLOK_TOKEN
+      }, API_URL)
+
+      const response = await client.get('spaces')
+      const spaces = response.data.spaces
+
+      const exists = spaces.filter(space => space.name === TEST_NAME)
+
+      expect(exists.length).toBe(1)
+
+      await client.delete(`spaces/${exists[0].id}`)
+    }
+  })
+})
