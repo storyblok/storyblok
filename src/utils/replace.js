0afd1be7883646af78be229b2f1c293ebee50a6d
Emanuel Souza <emanuelgdesouza@gmail.com>
2019-11-11 18:25:17 -0300

    refactor: refactor replace function to use Promise

--- src/utils/replace.js
+++ src/utils/replace.js
@@ -1,17 +1,23 @@
 const fs = require('fs')
 
 module.exports = (file, replacements) => {
-  fs.readFile(file, 'utf8', function (err, data) {
-    if (err) {
-      return console.log(err)
-    }
+  return new Promise((resolve, reject) => {
+    fs.readFile(file, 'utf8', function (err, data) {
+      if (err) {
+        return reject(err)
+      }
 
-    for (const from in replacements) {
-      data = data.replace(from, replacements[from])
-    }
+      for (const from in replacements) {
+        data = data.replace(from, replacements[from])
+      }
 
-    fs.writeFile(file, data, 'utf8', function (err) {
-      if (err) return console.log(err)
+      fs.writeFile(file, data, 'utf8', function (err) {
+        if (err) {
+          return reject(err)
+        }
+
+        return resolve(true)
+      })
     })
   })
 }
