924d6a439e2c1fe7f0ac95b09382435f91f45a35
Emanuel Souza <emanuelgdesouza@gmail.com>
2019-11-11 18:24:59 -0300

    refactor: refactor last-step script to use Promise

--- src/utils/last-step.js
+++ src/utils/last-step.js
@@ -1,88 +1,108 @@
+/* eslint-disable camelcase */
+
 const fs = require('fs')
 const chalk = require('chalk')
 const ghdownload = require('github-download')
 const replace = require('./replace')
 
-const lastStep = answers => {
-  var regex = /\[(.*?)\]/
-  var gitRepo = ''
+const getFinalStep = type => {
+  if (type === 'Fieldtype' || type === 'quickstart') {
+    return 'npm run dev'
+  }
 
-  if (answers.type === 'Theme (Storyrenderer/Hosted)' || answers.type === 'Boilerplate (Selfhosted)') {
-    if (answers.custom_theme_url) {
-      gitRepo = answers.custom_theme_url + '.git'
+  return 'gulp'
+}
+
+const getRepository = (type, theme, custom_theme_url) => {
+  const regex = /\[(.*?)\]/
+  if (type === 'Theme (Storyrenderer/Hosted)' || type === 'Boilerplate (Selfhosted)') {
+    if (custom_theme_url) {
+      return custom_theme_url + '.git'
     } else {
-      gitRepo = regex.exec(answers.theme)[1] + '.git'
+      return regex.exec(theme)[1] + '.git'
     }
   }
 
-  if (answers.type === 'Fieldtype') {
-    gitRepo = 'https://github.com/storyblok/storyblok-fieldtype.git'
+  if (type === 'Fieldtype') {
+    return 'https://github.com/storyblok/storyblok-fieldtype.git'
   }
 
-  if (answers.type === 'quickstart') {
-    gitRepo = 'https://github.com/storyblok/quickstart.git'
-  }
+  return 'https://github.com/storyblok/quickstart.git'
+}
 
-  var outputDir = './' + answers.name
+const lastStep = answers => {
+  return new Promise((resolve, reject) => {
+    const {
+      type,
+      theme,
+      spaceId,
+      name,
+      custom_theme_url,
+      themeToken,
+      spaceDomain,
+      loginToken
+    } = answers
 
-  console.log(chalk.green('✓') + ' - The github repository ' + gitRepo + ' will be cloned now...')
-  ghdownload(gitRepo, outputDir)
-    .on('error', function (err) {
-      if (err.code === 'ENOTEMPTY') {
-        console.log()
-        console.log(chalk.red('  Oh Snap! It seems that you already have a project with the name: ' + answers.name))
-        console.log()
-      } else {
-        console.log()
-        console.log(chalk.red('  We never had this kind of issue - Sorry for that!'))
-        console.log(chalk.red('  Could you send us the error below as a stackoverflow question?'))
-        console.log(chalk.red('  That would be great! :)'))
-        console.log(chalk.red('  Don\'t forget to mark it with the tag `storyblok` so will can find it.'))
-        console.log()
-        console.error(err)
-        process.exit(0)
-      }
-    })
-    .on('end', function () {
-      var finalStep = 'gulp'
+    const gitRepo = getRepository(type, theme, custom_theme_url)
+    const outputDir = './' + name
 
-      if (answers.type === 'Fieldtype' || answers.type === 'quickstart') {
-        finalStep = 'npm run dev'
-      }
+    console.log(chalk.green('✓') + ' - The github repository ' + gitRepo + ' will be cloned now...')
 
-      console.log(chalk.green('✓') + ' - ' + chalk.white('Your Storyblok project is ready for you!'))
-      console.log()
-      console.log(chalk.white('  Execute the following command to start Storyblok:'))
-      console.log(chalk.cyan('  cd ./' + answers.name + ' && npm install && ' + finalStep))
-      console.log()
-      console.log(chalk.white('  If you need more help, just try asking a question on stackoverflow'))
-      console.log(chalk.white('  with the [storyblok] tag or live-chat with us on www.storyblok.com'))
-      console.log()
-
-      if (answers.type === 'Theme (Storyrenderer/Hosted)' || answers.type === 'quickstart') {
-        fs.renameSync(outputDir + '/_token.js', outputDir + '/token.js')
-
-        if (answers.themeToken) {
-          replace(outputDir + '/token.js', { INSERT_TOKEN: answers.themeToken })
+    ghdownload(gitRepo, outputDir)
+      .on('error', function (err) {
+        if (err.code === 'ENOTEMPTY') {
+          console.log(chalk.red('  Oh Snap! It seems that you already have a project with the name: ' + name))
+          reject(new Error('This repository already has been cloned'))
+          process.exit(0)
         }
 
-        var spaceConfig = {}
+        console.log(chalk.red('X We never had this kind of issue - Sorry for that!'))
+        console.log(chalk.red('X Could you send us the error below as a stackoverflow question?'))
+        console.log(chalk.red('X That would be great! :)'))
+        console.log(chalk.red('X Don\'t forget to mark it with the tag `storyblok` so will can find it.'))
 
-        if (answers.spaceId) {
-          spaceConfig.INSERT_SPACE_ID = answers.spaceId
+        return reject(err)
+      })
+      .on('end', async function () {
+        const finalStep = getFinalStep(type)
+
+        console.log(chalk.green('✓') + ' - ' + chalk.white('Your Storyblok project is ready for you!'))
+        console.log(chalk.white('  Execute the following command to start Storyblok:'))
+        console.log(chalk.cyan('  cd ./' + name + ' && npm install && ' + finalStep))
+        console.log(chalk.white('  If you need more help, just try asking a question on stackoverflow'))
+        console.log(chalk.white('  with the [storyblok] tag or live-chat with us on www.storyblok.com'))
+
+        try {
+          if (type === 'Theme (Storyrenderer/Hosted)' || type === 'quickstart') {
+            fs.renameSync(outputDir + '/_token.js', outputDir + '/token.js')
+
+            if (themeToken) {
+              await replace(outputDir + '/token.js', { INSERT_TOKEN: themeToken })
+            }
+
+            var spaceConfig = {}
+
+            if (spaceId) {
+              spaceConfig.INSERT_SPACE_ID = spaceId
+            }
+
+            if (spaceDomain) {
+              spaceConfig.INSERT_YOUR_DOMAIN = spaceDomain
+            }
+
+            if (loginToken) {
+              spaceConfig.TEMP_QUICKSTART_TOKEN = loginToken
+            }
+
+            await replace(outputDir + '/config.js', spaceConfig)
+
+            return resolve(true)
+          }
+        } catch (e) {
+          return reject(new Error('An error occurred when finish the download repository task ' + e.message))
         }
-
-        if (answers.spaceDomain) {
-          spaceConfig.INSERT_YOUR_DOMAIN = answers.spaceDomain
-        }
-
-        if (answers.loginToken) {
-          spaceConfig.TEMP_QUICKSTART_TOKEN = answers.loginToken
-        }
-
-        replace(outputDir + '/config.js', spaceConfig)
-      }
-    })
+      })
+  })
 }
 
 module.exports = lastStep
