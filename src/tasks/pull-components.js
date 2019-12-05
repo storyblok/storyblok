97d6d784e9c665dc0dd8b9788d152409fc6317b2
Emanuel Souza <emanuelgdesouza@gmail.com>
2019-12-05 16:40:04 -0300

    feat: add component_group_name property into pull-components task result file

--- src/tasks/pull-components.js
+++ src/tasks/pull-components.js
@@ -2,35 +2,60 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 /**
+ * @method getNameFromComponentGroups
+ * @param  {Array<Object>} groups
+ * @param  {String} uuid
+ * @return {String}
+ */
+const getNameFromComponentGroups = (groups, uuid) => {
+  const exists = groups.filter(group => group.uuid === uuid)
+
+  if (exists.length) {
+    return exists[0].name
+  }
+
+  return ''
+}
+
+/**
  * @method pullComponents
  * @param  {Object} api
  * @param  {Object} options { space: Number }
  * @return {Promise<Object>}
  */
-const pullComponents = (api, options) => {
+const pullComponents = async (api, options) => {
   const { space } = options
 
-  return api
-    .getComponents()
-    .then(components => {
-      const file = `components.${space}.json`
-      const data = JSON.stringify({ components }, null, 2)
+  try {
+    const componentGroups = await api.getComponentGroups()
 
-      console.log(`${chalk.green('✓')} We've saved your components in the file: ${file}`)
+    const components = await api.getComponents()
 
-      fs.writeFile(`./${file}`, data, err => {
-        if (err) {
-          Promise.reject(err)
-          return
-        }
-
-        Promise.resolve(file)
-      })
+    components.forEach(component => {
+      const groupUuid = component.component_group_uuid
+      if (groupUuid) {
+        const group = getNameFromComponentGroups(componentGroups, groupUuid)
+        component.component_group_name = group
+      }
     })
-    .catch(err => {
-      console.error(`${chalk.red('X')} An error ocurred in pull-components task when load components data`)
-      return Promise.reject(new Error(err))
+
+    const file = `components.${space}.json`
+    const data = JSON.stringify({ components }, null, 2)
+
+    console.log(`${chalk.green('✓')} We've saved your components in the file: ${file}`)
+
+    fs.writeFile(`./${file}`, data, err => {
+      if (err) {
+        Promise.reject(err)
+        return
+      }
+
+      Promise.resolve(file)
     })
+  } catch (e) {
+    console.error(`${chalk.red('X')} An error ocurred in pull-components task when load components data`)
+    return Promise.reject(new Error(e))
+  }
 }
 
 module.exports = pullComponents
