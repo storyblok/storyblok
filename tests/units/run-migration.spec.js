ab092411af941172c43a5cec329573c2507e5b5d
Emanuel Souza <emanuelgdesouza@gmail.com>
2020-07-09 15:44:59 -0300

    refactor: refactor tests to run-migration command

--- tests/units/run-migration.spec.js
+++ tests/units/run-migration.spec.js
@@ -100,125 +100,6 @@ describe('testing runMigration', () => {
     })
   })
 
-  describe('when the execution is executed as expected', () => {
-    const FAKE_API = {
-      getStories: jest.fn(() => Promise.resolve(FAKE_STORIES())),
-      getSingleStory: jest.fn(id => {
-        const data = FAKE_STORIES().filter(story => story.id === id)[0] || {}
-        return Promise.resolve(data)
-      }),
-      put: jest.fn(() => {})
-    }
-
-    const opt = {
-      migrationPath
-    }
-
-    beforeEach(() => {
-      require('fs-extra').__clearMockFiles()
-      require('fs-extra').__setMockFiles({
-        [getFilePath('subtitle')]: 'module.exports = {}'
-      })
-    })
-
-    afterEach(() => {
-      jest.clearAllMocks()
-    })
-
-    it('returns executed = true', async () => {
-      expect(
-        await runMigration(FAKE_API, 'teaser', 'subtitle', opt)
-      ).toEqual({
-        executed: true
-      })
-    })
-
-    it('execute migration function for each component found', async () => {
-      try {
-        await runMigration(FAKE_API, 'teaser', 'subtitle', opt)
-      } catch (e) {
-        console.error(e)
-      }
-
-      // check how many times the migration function was executed
-      expect(migrationFile.mock.calls.length).toBe(2)
-    })
-
-    it('execute api.put with the story change', async () => {
-      try {
-        await runMigration(FAKE_API, 'teaser', 'subtitle', opt)
-      } catch (e) {
-        console.error(e)
-      }
-
-      // check how many times the put function was executed
-      expect(FAKE_API.put.mock.calls.length).toBe(2)
-
-      // the first execution
-      const firstExecution = FAKE_API.put.mock.calls[0]
-      expect(firstExecution[0]).toBe('stories/0')
-
-      const firstExecutionContent = firstExecution[1].story.content
-      expect(firstExecutionContent).toStrictEqual({
-        _uid: '4bc98f32-6200-4176-86b0-b6f2ea14be6f',
-        body: [
-          {
-            _uid: '111781de-3174-4f61-8ae3-0b653085a582',
-            headline: 'My About Page',
-            component: 'teaser',
-            subtitle: 'Hey There!' // adds the field with value
-          }
-        ],
-        component: 'page'
-      })
-
-      // the second execution
-      const secondExecution = FAKE_API.put.mock.calls[1]
-      expect(secondExecution[0]).toBe('stories/1')
-
-      const secondExecutionContent = secondExecution[1].story.content
-      expect(secondExecutionContent).toStrictEqual({
-        _uid: '4bc98f32-6200-4176-86b0-b6f2ea14be6f',
-        body: [
-          {
-            _uid: '111781de-3174-4f61-8ae3-0b653085a582',
-            headline: 'Hello World!',
-            component: 'teaser',
-            subtitle: 'Hey There!'
-          },
-          {
-            _uid: '2aae2a9b-df65-4572-be4c-e4460d81e299',
-            columns: [
-              {
-                _uid: 'cd6efa74-31c3-47ec-96d2-91111f2a6c7c',
-                name: 'Feature 1',
-                component: 'feature',
-                image_data: '//a.storyblok.com/f/67249/1024x512/64e7272404/headless.png'
-              },
-              {
-                _uid: 'e994cb3e-0f2b-40a7-8db1-f3e456e7b868',
-                name: 'Feature 2',
-                component: 'feature'
-              },
-              {
-                _uid: '779ee42f-a856-405c-9e34-5b49380ae4fe',
-                name: 'Feature 3',
-                component: 'feature'
-              },
-              {
-                _uid: 'a315e9a7-4a0c-4cc5-b393-572533e8bf87',
-                image: '//a.storyblok.com/f/67249/1024x512/64e7272404/headless.png',
-                component: 'my-image'
-              }
-            ],
-            component: 'grid'
-          }
-        ],
-        component: 'page'
-      })
-    })
-  })
-
   describe('when execute with isDryrun enable', () => {
     const FAKE_API = {
       getStories: jest.fn(() => Promise.resolve(FAKE_STORIES())),
@@ -313,6 +194,139 @@ describe('testing runMigration', () => {
 
       // check how many times the put function was executed
       expect(FAKE_API.put.mock.calls.length).toBe(0)
+    })
+  })
+
+  describe('when the execution is executed as expected', () => {
+    const FAKE_API = {
+      getStories: jest.fn(() => Promise.resolve(FAKE_STORIES())),
+      getSingleStory: jest.fn(id => {
+        const data = FAKE_STORIES().filter(story => story.id === id)[0] || {}
+        return Promise.resolve(data)
+      }),
+      put: jest.fn(() => {})
+    }
+
+    const opt = {
+      migrationPath
+    }
+
+    beforeEach(() => {
+      require('fs-extra').__clearMockFiles()
+      require('fs-extra').__setMockFiles({
+        [getFilePath('subtitle')]: 'module.exports = {}'
+      })
+    })
+
+    afterEach(() => {
+      jest.clearAllMocks()
+    })
+
+    it('returns executed = true', async () => {
+      expect(
+        await runMigration(FAKE_API, 'teaser', 'subtitle', opt)
+      ).toEqual({
+        executed: true
+      })
+    })
+
+    it('execute migration function for each component found', async () => {
+      try {
+        await runMigration(FAKE_API, 'teaser', 'subtitle', opt)
+      } catch (e) {
+        console.error(e)
+      }
+
+      // check how many times the migration function was executed
+      expect(migrationFile.mock.calls.length).toBe(2)
+    })
+
+    it('execute api.put with the story change', async () => {
+      try {
+        await runMigration(FAKE_API, 'teaser', 'subtitle', opt)
+      } catch (e) {
+        console.error(e)
+      }
+
+      // check how many times the put function was executed
+      expect(FAKE_API.put.mock.calls.length).toBe(2)
+
+      // the first execution
+      const firstExecution = FAKE_API.put.mock.calls[0]
+      const firstExecutiontionPath = firstExecution[0]
+      const firstExecutiontionPayload = firstExecution[1]
+
+      // path
+      expect(firstExecutiontionPath).toBe('stories/0')
+
+      // payload
+      const firstExecutionContent = firstExecutiontionPayload.story.content
+      expect(firstExecutionContent).toStrictEqual({
+        _uid: '4bc98f32-6200-4176-86b0-b6f2ea14be6f',
+        body: [
+          {
+            _uid: '111781de-3174-4f61-8ae3-0b653085a582',
+            headline: 'My About Page',
+            component: 'teaser',
+            subtitle: 'Hey There!' // adds the field with value
+          }
+        ],
+        component: 'page'
+      })
+
+      expect(firstExecutiontionPayload.force_update).toBe('1')
+
+      // the second execution
+      const secondExecution = FAKE_API.put.mock.calls[1]
+      const secondExecutionPath = secondExecution[0]
+      const secondExecutionPayload = secondExecution[1]
+
+      // path
+      expect(secondExecutionPath).toBe('stories/1')
+
+      // content
+      const secondExecutionContent = secondExecutionPayload.story.content
+      expect(secondExecutionContent).toStrictEqual({
+        _uid: '4bc98f32-6200-4176-86b0-b6f2ea14be6f',
+        body: [
+          {
+            _uid: '111781de-3174-4f61-8ae3-0b653085a582',
+            headline: 'Hello World!',
+            component: 'teaser',
+            subtitle: 'Hey There!'
+          },
+          {
+            _uid: '2aae2a9b-df65-4572-be4c-e4460d81e299',
+            columns: [
+              {
+                _uid: 'cd6efa74-31c3-47ec-96d2-91111f2a6c7c',
+                name: 'Feature 1',
+                component: 'feature',
+                image_data: '//a.storyblok.com/f/67249/1024x512/64e7272404/headless.png'
+              },
+              {
+                _uid: 'e994cb3e-0f2b-40a7-8db1-f3e456e7b868',
+                name: 'Feature 2',
+                component: 'feature'
+              },
+              {
+                _uid: '779ee42f-a856-405c-9e34-5b49380ae4fe',
+                name: 'Feature 3',
+                component: 'feature'
+              },
+              {
+                _uid: 'a315e9a7-4a0c-4cc5-b393-572533e8bf87',
+                image: '//a.storyblok.com/f/67249/1024x512/64e7272404/headless.png',
+                component: 'my-image'
+              }
+            ],
+            component: 'grid'
+          }
+        ],
+        component: 'page'
+      })
+
+      expect(secondExecutionPayload.force_update).toBe('1')
     })
   })
 })
