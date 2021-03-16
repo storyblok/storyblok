661c932cdbe62b7fd30474b13e1d8e7f718e3927
Christian Zoppi <me@christianzoppi.com>
2021-03-16 18:28:26 +0100

    Push and pull presets

--- src/tasks/push-components.js
+++ src/tasks/push-components.js
@@ -31,10 +31,18 @@ module.exports = (api, options) => {
   }
 
   const data = fs.readFileSync(source, 'utf8')
+  let presetsData
+  if (fs.existsSync(source.replace('components.', 'presets.'))) {
+    presetsData = fs.readFileSync(source.replace('components.', 'presets.'), 'utf8')
+  }
   if (data) {
     const body = JSON.parse(data)
+    let presetsBody
+    if (presetsData) {
+      presetsBody = JSON.parse(presetsData)
+    }
     if (body.components) {
-      return push(api, body.components)
+      return push(api, body.components, presetsBody ? presetsBody.presets : [])
     }
 
     console.error(`${chalk.red('X')} Can not load json file from ${source}`)
@@ -45,13 +53,65 @@ module.exports = (api, options) => {
   return Promise.reject(new Error('Can not push invalid json - please provide a valid json file'))
 }
 
-const push = async (api, components) => {
+const getComponentPresets = (component = {}, presets) => {
+  console.log(`${chalk.green('-')} Get presets from component ${component.name}`)
+
+  return presets.filter(preset => {
+    return preset.component_id === component.id
+  })
+}
+
+const filterPresetsFromTargetComponent = (presets, targetPresets) => {
+  console.log(chalk.blue('-') + ' Checking target presets to sync...')
+  const targetPresetsNames = targetPresets.map(preset => preset.name)
+  const newPresets = presets.filter(preset => !targetPresetsNames.includes(preset.name))
+  const updatePresetsSource = presets.filter(preset => targetPresetsNames.includes(preset.name))
+  const updatePresets = updatePresetsSource.map(source => {
+    const target = targetPresets.find(target => target.name === source.name)
+    return Object.assign({}, source, target, { image: source.image })
+  })
+
+  return {
+    newPresets,
+    updatePresets
+  }
+}
+
+const createPresets = async (api, presets = [], componentId, method = 'post') => {
+  const presetsSize = presets.length
+  console.log(`${chalk.blue('-')} Pushing ${presetsSize} ${method === 'post' ? 'new' : 'existing'} presets`)
+
+  try {
+    for (let i = 0; i < presetsSize; i++) {
+      const presetData = presets[i]
+      const presetId = method === 'put' ? `/${presetData.id}` : ''
+
+      await api[method](`presets${presetId}`, {
+        preset: {
+          name: presetData.name,
+          component_id: componentId,
+          preset: presetData.preset,
+          image: presetData.image
+        }
+      })
+    }
+
+    console.log(`${chalk.green('✓')} ${presetsSize} presets sync`)
+  } catch (e) {
+    console.error('An error ocurred when save the presets' + e.message)
+
+    return Promise.reject(e)
+  }
+}
+
+const push = async (api, components, presets) => {
   try {
     const componentGroups = await api.getComponentGroups()
 
     const apiComponents = await api.getComponents()
 
     for (var i = 0; i < components.length; i++) {
+      const componentPresets = getComponentPresets(components[i], presets)
       delete components[i].id
       delete components[i].created_at
 
@@ -86,7 +146,7 @@ const push = async (api, components) => {
       if (exists.length > 0) {
         const { id, name } = exists[0]
         console.log(`${chalk.blue('-')} Updating component ${name}...`)
-
+        const componentTarget = await api.get(`components/${id}`)
         try {
           await api.put(`components/${id}`, {
             component: components[i]
@@ -97,13 +157,26 @@ const push = async (api, components) => {
           console.error(`${chalk.red('X')} An error occurred when update component ${name}`)
           console.error(e.message)
         }
+        const presetsToSave = filterPresetsFromTargetComponent(
+          componentPresets || [],
+          componentTarget.data.component.all_presets || []
+        )
+        if (presetsToSave.newPresets.length) {
+          await createPresets(api, presetsToSave.newPresets, componentTarget.data.component.id, 'post')
+        }
+        if (presetsToSave.updatePresets.length) {
+          await createPresets(api, presetsToSave.updatePresets, componentTarget.data.component.id, 'put')
+        }
       } else {
         const { name } = components[i]
         console.log(`${chalk.blue('-')} Creating component ${name}...`)
         try {
-          await api.post('components', {
+          const componentRes = await api.post('components', {
             component: components[i]
           })
+          if (componentPresets) {
+            createPresets(api, componentPresets, componentRes.data.component.id)
+          }
           console.log(`${chalk.green('✓')} Component ${name} has been updated in Storyblok!`)
         } catch (e) {
           console.error(`${chalk.red('X')} An error occurred when create component`)
