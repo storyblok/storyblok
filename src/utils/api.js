9797cb1211d7a82c19b6cc5e0c959b3afbb44591
Ricardo Reis <ricardo@significa.co>
2022-06-02 12:07:20 +0100

    feat: add pull languages

--- src/utils/api.js
+++ src/utils/api.js
@@ -147,6 +147,15 @@ module.exports = {
       .catch(err => Promise.reject(err))
   },
 
+  getSpaceOptions () {
+    const client = this.getClient()
+
+    return client
+      .get(this.getPath(''))
+      .then((data) => data.data.space.options || {})
+      .catch((err) => Promise.reject(err))
+  },
+
   getComponents () {
     const client = this.getClient()
 
